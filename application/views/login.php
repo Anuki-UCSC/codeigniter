76809d14ac34ad9e7925172d7eb35731eb06c953
Anuki-UCSC <alwis.anuki@gmail.com>
2021-05-03 22:46:42 +0530

    login view

---
+++ application/views/login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body style="padding:10%; background:url('https://i.pinimg.com/originals/f3/0b/30/f30b302d07d3cc4a8cbbe9ab00dd7246.jpg'); background-size: 100% auto;">
+<form style="margin-left:20%;margin-right:20%; padding:30px; background-color:rgb(234, 191, 202,0.8); ">
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  <div class="form-group form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">remember</label>
+  </div>
+  <button type="submit" class="btn btn-primary" style="margin-left:40%; padding-right:40px;padding-left:40px;">Login</button>
+</form>
+</body>
+</html>
