0321dc73bdbf7837c82a97432e5001f754c802d3
Anuki-UCSC <alwis.anuki@gmail.com>
2021-05-03 22:46:57 +0530

    register view

---
+++ application/views/register.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+
+
+
+<div class="row" >
+  <div class="col-md-2">&nbsp;</div>
+  <div class="col-md-8">
+    <div class="row space-16" >&nbsp;</div>
+    <div class="row" >
+      <div class="col-sm-4" style="background-color:yellow;">
+        <div class="thumbnail">
+          <div class="caption text-center" onclick="location.href='https://flow.microsoft.com/en-us/connectors/shared_slack/slack/'">
+            <div class="position-relative">
+              <img src="https://az818438.vo.msecnd.net/icons/slack.png" style="width:72px;height:72px;" />
+            </div>
+            <h4 id="thumbnail-label"><a href="https://flow.microsoft.com/en-us/connectors/shared_slack/slack/" target="_blank">Microsoft Slack</a></h4>
+            <p><i class="glyphicon glyphicon-user light-red lighter bigger-120"></i>&nbsp;Auditor</p>
+            <div class="thumbnail-description smaller">Slack is a team communication tool, that brings together all of your team communications in one place, instantly searchable and available wherever you go.</div>
+          </div>
+          <div class="caption card-footer text-center">
+            <ul class="list-inline">
+              <li><i class="people lighter"></i>&nbsp;7 Active Users</li>
+              <li></li>
+              <li><i class="glyphicon glyphicon-envelope lighter"></i><a href="#">&nbsp;Help</a></li>
+            </ul>
+          </div>
+        </div>
+
+        
+      </div>
+
+      <div class="col-sm-4" >
+        <div class="thumbnail">
+          <div class="caption text-center" onclick="location.href='https://flow.microsoft.com/en-us/connectors/shared_slack/slack/'">
+            <div class="position-relative">
+              <img src="https://az818438.vo.msecnd.net/icons/slack.png" style="width:72px;height:72px;" />
+            </div>
+            <h4 id="thumbnail-label"><a href="https://flow.microsoft.com/en-us/connectors/shared_slack/slack/" target="_blank">Microsoft Slack</a></h4>
+            <p><i class="glyphicon glyphicon-user light-red lighter bigger-120"></i>&nbsp;Auditor</p>
+            <div class="thumbnail-description smaller">Slack is a team communication tool, that brings together all of your team communications in one place, instantly searchable and available wherever you go.</div>
+          </div>
+          <div class="caption card-footer text-center">
+            <ul class="list-inline">
+              <li><i class="people lighter"></i>&nbsp;7 Active Users</li>
+              <li></li>
+              <li><i class="glyphicon glyphicon-envelope lighter"></i><a href="#">&nbsp;Help</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+     
+
+ 
+
+    </div>
+    <div class="col-md-2">&nbsp;</div>
+  </div>
+</div>
+
+
+
+
+</body>
+</html>
